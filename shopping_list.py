6f69df6a3d8c27d6106c87e80127f9ce37636272
joczarnocka <42293202+joczarnocka@users.noreply.github.com>
2022-07-23 10:25:09 +0200

    Add iteration through shopping list dictionary

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,6 @@ shopping_dict = {
      'piekarnia': ['chleb', 'bułki','pączek'],
      'warzywniak':['marchew', 'seler','rukola']
 }
+
+for  k, v in shopping_dict.items():
+    print(f"Idę do {k}, kupuję tu następujące rzeczy: {v}")
