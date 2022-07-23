f5706c942b354cc16afd44094f7dc299feee4fdc
joczarnocka <42293202+joczarnocka@users.noreply.github.com>
2022-07-23 10:28:30 +0200

    Add printing total number of products

--- shopping_list.py
+++ shopping_list.py
@@ -4,6 +4,10 @@ shopping_dict = {
      'warzywniak':['marchew', 'seler','rukola']
 }
 
+total_number = 0
 for  k, v in shopping_dict.items():
     v2 = [ item.capitalize() for item in v]
+    total_number += len(v2)
     print(f"Idę do {k.capitalize()}, kupuję tu następujące rzeczy: {v2}")
+
+print(f"W sumie kupuję {total_number} produktów.");
